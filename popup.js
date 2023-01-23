3e5ec23cc0e7ae335f0d766c3dd96ef18d504467
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2023-01-23 11:43:26 -0700

    Added check to ensure interval from user is number

--- popup.js
+++ popup.js
@@ -3,14 +3,31 @@ var saveButton = document.getElementById('save')
 saveButton.addEventListener('click', ()=>{
 
     var newInterval = document.getElementById('interval').value
-    console.log(newInterval)
-    // Send value to background.js
-    chrome.runtime.sendMessage({action: 'update', interval:newInterval})
-    var message = document.getElementById('message')
-    message.classList = "show"
-   setTimeout(()=>{
-    message.classList = "hidden"
-   }, "3000");
+    
+    // Check if not a number
+    if( isNaN(parseInt(newInterval))){
+
+        var message = document.getElementById('message')
+        message.innerHTML = "Not a valid interval"
+        message.classList = "show"
+       setTimeout(()=>{
+        message.classList = "hidden"
+       }, "3000");
+
+    }else{
+        console.log(newInterval)
+
+        var message = document.getElementById('message')
+        message.innerHTML = "Interval Saved & Updated"
+        // Send value to background.js
+        chrome.runtime.sendMessage({action: 'update', interval:newInterval})
+        var message = document.getElementById('message')
+        message.classList = "show"
+       setTimeout(()=>{
+        message.classList = "hidden"
+       }, "3000");
+    }
+
 })
 
 
