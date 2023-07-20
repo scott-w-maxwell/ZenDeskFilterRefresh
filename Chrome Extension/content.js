6a5a6b3063d24a37deecfc4cc940fa08ec7e0f02
= <=>
2023-07-20 08:43:07 -0700

    Wrapped refresh click in condition for button to exist

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -6,5 +6,7 @@ if(typeof zendesk_refresh != 'undefined'){
 // set interval to click button
 zendesk_refresh = setInterval(function(){
 	let refresh = $("#main_panes button[data-test-id='views_views-list_header-refresh']"); // Replace with regular JS TODO
-	refresh.click()
+	if(refresh !== undefined){
+		refresh.click();
+	}
 }, 10 * 1000);
